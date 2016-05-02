148f771d7449b070f56ab172410b3889bc87dd48
Yvonne Hayes <queyvonne@gmail.com>
2016-05-02 13:23:44 -0700

    Added a HOF closure. Can now display different messages with the Username. Can make that dpendent on weather as soon as we have that data

--- js/form.js
+++ js/form.js
@@ -8,7 +8,22 @@ $(document).ready(function(){
   });
 });
 
-function useName (currentUser){
-  console.log(currentUser);
-  $('#output').text('Hey ' + currentUser + ', what a great day for biking!');
+function useName(currentUser, goodDayOrBadDay) { //params of currentUser and what kind of day it is
+  var makeGoodorBad = function(goodOrBad) {  //makeGoodorBad - to display correct kind of day later
+    var node = document.createElement('p'); //creating proper elements and appending to DOM
+    var textnode = document.createTextNode('Hey ' + currentUser + ', ' + goodOrBad);
+    node.appendChild(textnode);
+    document.getElementById('output2').appendChild(node);
+  };
+  return makeGoodorBad;
+}
+
+  //here cometh the Closure
+function message (currentUser){
+
+  var goodDay = useName(currentUser, 'itsgood');
+  goodDay('what a great day for biking!');
+
+  var badDay = useName(currentUser, 'itsbad');
+  badDay('maybe leave the bike at home today....');
 };
