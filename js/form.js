7bf987fd6c2af916a896a84c5cc462577ad7f844
Yvonne Hayes <queyvonne@gmail.com>
2016-05-03 15:46:46 -0700

    trying to add tooltips as user alerts if invalid input

--- js/form.js
+++ js/form.js
@@ -94,3 +94,33 @@ function validateLocation(e){
     localStorage.setItem('zipNumber', zip);
   };
 };
+
+
+//validation messages and tooltips
+
+$('form').validate({
+
+  showErrors: function(errorMap, errorList){
+
+    $each(this.validElements(), function(index, element){
+      var $element = $(element);
+
+      $element.data('title', '')
+        .removeClass('error')
+        .tooltip('destroy');
+    });
+
+    $.each(errorList, function (index,error) {
+      var $element = $(error.element);
+
+      $element.tooltip('destroy')
+        .data('title', error.message)
+        .addClass('error')
+        .tooltip();
+    });
+  },
+
+  submitHandler: function(form) {
+    alert('This is cool');
+  }
+});
