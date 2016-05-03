a32f8feebd40299912cf7bbb77661d3f86e0a4ea
Kate Sowles <katesowles@gmail.com>
2016-05-03 13:49:41 -0700

    adding scroll to functionality instead of showing/hiding pages

--- js/welcomeController.js
+++ js/welcomeController.js
@@ -2,8 +2,17 @@
   var welcomeController = {};
 
   welcomeController.index = function() {
-    $('section').hide();
-    $('#welcome').show();
+    // $('section').hide();
+    // $('#welcome').show();
+
+    $(document).ready(function() {
+      $('#toWelcome').click(function() {
+        $('html, body').animate({
+          scrollTop: $('#welcome').offset().top
+        }, 2000);
+      });
+    });
+
     $('nav a').removeClass('viewing');
     $('nav #toWelcome').addClass('viewing');
   };
