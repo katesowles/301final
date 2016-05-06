e437b3ec78f10458fc30c427e2891e33ab6f01ce
Kate Sowles <katesowles@gmail.com>
2016-05-06 11:02:47 -0700

    pushing up final changes

--- js/view.js
+++ js/view.js
@@ -14,8 +14,11 @@
   };
 
   view.deleteFromDom = function (ctx) {
-    var domElement = '#record' + ctx.params.id;
-    $(domElement).remove();
+    var domDashboard = '#record' + ctx.params.id;
+    var domConfig = '#recordB' + ctx.params.id;
+    $(domDashboard).remove();
+    $(domConfig).remove();
+    history.pushState({}, null, '/');
   };
 
   view.adjustSectionHeight();
